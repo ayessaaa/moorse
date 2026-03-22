e9df45607c92042bcc726a12f1affdb04d9d9948
yessa <108473583+ayessaaa@users.noreply.github.com>
2026-03-22 22:41:44 +0800

    setup

---
+++ moorse/moorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  moorseApp.swift
+//  moorse
+//
+//  Created by yessa on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct moorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moorseTests/moorseTests.swift
@@ -0,0 +1,17 @@
+//
+//  moorseTests.swift
+//  moorseTests
+//
+//  Created by yessa on 3/22/26.
+//
+
+import Testing
+@testable import moorse
+
+struct moorseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
