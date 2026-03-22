9db2ed5cba271516bf8f6a0e1fbe908d7bb9aa4a
yessa <108473583+ayessaaa@users.noreply.github.com>
2026-03-23 00:17:21 +0800

    feat(menu): started working on the menu page

--- moorse/ContentView.swift
+++ moorse/ContentView.swift
@@ -6,17 +6,67 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("LogoTitle")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 30)
+
+            Image("LogoSubtitle")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 30)
+                .padding(.top, -5)
+
+            Button(action: {
+
+            }) {
+                HStack {
+                    Text("Get Started")
+                        .padding(.trailing, 5)
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 5)
+                .shadow(radius: 0, x: 0, y: 5)
+
+            }
+
+            .font(Font.custom("LondrinaSolid-Regular", size: 26))
+            .font(.title)
+            .foregroundStyle(.colorBeige100)
+            //                .background(.colorMaroon300)
+            .background(
+                Image("ButtonBG")
+                    .resizable()
+                    .scaledToFill()
+            )
+            .clipShape(Capsule())
+            .shadow(color: .colorMaroon400, radius: 0, x: 0, y: 5)
+            .buttonStyle(.bordered)
+
+            .overlay(
+                Capsule()
+                    .stroke(Color.colorOrange200.opacity(0.25), lineWidth: 4)
+                    .blur(radius: 0)
+                    .offset(y: -2)
+                    .mask(Capsule())
+            )
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.colorBeige100)
+        .ignoresSafeArea()
     }
+
 }
 
 #Preview {
