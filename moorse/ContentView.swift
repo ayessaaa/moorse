54cb9b27e7f632c7dfa4215dc1ef897c2bb355fd
yessa <108473583+ayessaaa@users.noreply.github.com>
2026-03-25 23:04:00 +0800

    chore(menu): moved current code to menu's own file

--- moorse/ContentView.swift
+++ moorse/ContentView.swift
@@ -6,87 +6,11 @@
 //
 
 import SwiftUI
-import UIKit
 
-struct CustomButtonStyle: ButtonStyle{
-    func makeBody(configuration: Configuration)-> some View {
-        configuration.label
-            .font(Font.custom("LondrinaSolid-Regular", size: 26))
-            .font(.title)
-            .foregroundStyle(.colorBeige100)
-            .background(
-                Image("ButtonBG")
-                    .resizable()
-                    .scaledToFill()
-            )
-            .clipShape(Capsule())
-        
-            .shadow(color: configuration.isPressed ? Color.colorMaroonOrange200.opacity(0) : Color.colorMaroon400 , radius: 0, x: 0, y: 5)
-            .buttonStyle(.bordered)
-
-            .overlay(
-                Capsule()
-                    .stroke(configuration.isPressed ? Color.colorMaroon400 : Color.colorOrange200.opacity(0.25), lineWidth: 5)
-                    .blur(radius: 0)
-                    .offset(y: configuration.isPressed ? 3 : -2)
-                    .mask(Capsule())
-            )
-            .overlay(
-                Capsule()
-                    .stroke(configuration.isPressed ? Color.colorOrange200.opacity(0.25) : Color.colorMaroon400, lineWidth: configuration.isPressed ? 5 : 0)
-                    .blur(radius: 0)
-                    .offset(y: configuration.isPressed ? -2 : 3)
-                    .mask(Capsule())
-            )
-            .offset(y: configuration.isPressed ? 5 : 0)
-    }
-}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("LogoTitle")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 30)
-
-            Image("LogoSubtitle")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 30)
-                .padding(.top, -5)
-                .padding(.bottom, 150)
-
-            Button(action: {
-
-            }) {
-                HStack {
-                    Text("Get Started")
-                        .padding(.trailing, 5)
-                    Image(systemName: "play.fill")
-                        .resizable()
-                        .frame(width: 16, height: 16)
-                }
-                .padding(.horizontal, 40)
-                .padding(.vertical, 10)
-                .shadow(radius: 0, x: 0, y: 5)
-
-            }
-            .buttonStyle(CustomButtonStyle())
-
-            
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(
-            Image("BG")
-                .resizable()
-                .scaledToFill()
-            
-        )
-        .ignoresSafeArea()
+        
         
     }
 
--- moorse/moorseApp.swift
+++ moorse/moorseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct moorseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Menu()
         }
     }
 }
